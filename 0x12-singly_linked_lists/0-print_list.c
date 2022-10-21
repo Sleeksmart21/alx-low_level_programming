f1e331f7f7e5530e34d32ace8cdf03f9ab0df785
Ogbonna Chigozie Benjamin <ogbonnachigozieben@gmail.com>
2022-10-20 21:46:57 -0700

    Just another correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -9,7 +7,6 @@
  *
  * Return: number of nodes in the list
  */
-
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
